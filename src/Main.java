2771fdae0361468befb9a669a4c811eb7edd90ac
nthndf-tech <nth.ndf@gmail.com>
2025-10-30 20:02:47 +0100

    Faltan 2

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,22 @@ public class Main {
 
     public static int[][] movimientoCaballo(int fila, int columna) {
         int[][] tablero = new int[8][8];
+
+        int[][] movimientos = {
+                {2, 1}, {2, -1}, {-2, 1}, {-2, -1},
+                {1, 2}, {1, -2}, {-1, 2}, {-1, -2},
+        };
+
+        for (int[] movimiento : movimientos) {
+            int nuevaFila = fila + movimiento[0];
+            int nuevaColumna = columna + movimiento[1];
+
+            if (nuevaFila != nuevaColumna) {
+                return tablero;
+            }
+        }
+
+
         return tablero;
     }
 
@@ -18,8 +34,19 @@ public class Main {
 
     public static int[][] movimientoTorre(int fila, int columna) {
         int[][] tablero = new int[8][8];
+
+        // Marca todas as posições possíveis da torre (na mesma fila e coluna)
+        for (int i = 0; i < 8; i++) {
+            // mesma coluna (mover na vertical)
+            if (i != fila) {                           //fila e columna é onde ta a torre no momento
+                tablero[i][columna] = 1;
+            }
+
+            // mesma fila (mover na horizontal)
+            if (i != columna) {
+                tablero[fila][i] = 1;
+            }
+        }
         return tablero;
     }
-
-
 }
